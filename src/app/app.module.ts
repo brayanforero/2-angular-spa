3eccfdf5930ab5283409c906056f56ee1e12f104
Brayan Forero <brayanroswll2802@gmail.com>
2021-10-04 09:16:16 -0400

    add new componentes hero and add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// SERVICES
+import { HeroesService } from './services/heroes.services';
 
+// COMPONENTS
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
-import { APPROUTING } from 'src/app.routes';
+import { HeroComponent } from './components/hero/hero.component';
+
+// ROUTES
+import { APP_ROUTING } from './app.routes';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, AboutComponent],
-  imports: [BrowserModule, APPROUTING],
-  providers: [],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroComponent,
+  ],
+  imports: [BrowserModule, APP_ROUTING],
+  providers: [HeroesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
